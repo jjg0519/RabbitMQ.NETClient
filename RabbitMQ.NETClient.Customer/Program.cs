40269a9bbe36a632ba1303935b746422eeaf25bf
Fanyuepan <fanpan26@126.com>
2016-05-24 11:53:50 +0800

    create project

---
+++ RabbitMQ.NETClient.Customer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Macrosage.RabbitMQ.Server.Config;
+using Macrosage.RabbitMQ.Server.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.NETClient.Customer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.For(0, RabbitMQConfig.ThreadCount, i =>
+            {
+                IMessageCustomer customer = new MessageCustomer("test");
+                customer.StartListening();
+                customer.ReceiveMessageCallback = message =>
+                {
+                    Console.WriteLine("接收到消息：" + message);
+                    return true;
+                };
+           });
+            Console.Read();
+        }
+    }
+}
